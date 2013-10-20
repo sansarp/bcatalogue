9e28fa7efc01486efc6c946f2f061351608cd916
spoudyal <subashpoudyal@lftechnology.com>
2013-10-20 09:32:47 +0545

    push to repo

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LfpQegSAAAAAIUUiNDd2OfWl140AFbdbk80IKCC'
+  config.private_key = '6LfpQegSAAAAAI3jq1YbAeM93R_BHSb9BNq1B59f'
+  # config.proxy = 'http://localhost:3000'
+end
